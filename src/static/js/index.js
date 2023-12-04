9e358fa5f9d1720f1197735758793e45e0b6a02e
Praveen Kumar Ramineni <52283525+Praveen-408@users.noreply.github.com>
2023-12-04 13:11:36 -0500

    Update index.js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,33 +1,132 @@
 const my_input = async () => {
-	const b = document.getElementsByClassName("container")[0];
-	const user_input = document.createElement("div");
+  const b = document.getElementsByClassName("chat-container")[0];
+  const user_input = document.createElement("div");
 
-  value = document.getElementsByClassName('input-box')[0].value
-  document.getElementsByClassName('input-box')[0].value = ""
-	user_input.className = "user-text";
-	const user_input_text = document.createTextNode(value);
-	user_input.appendChild(user_input_text);
-	b.appendChild(user_input);
+  value = document.getElementsByClassName("input-box")[0].value;
+  document.getElementsByClassName("input-box")[0].value = "";
+  user_input.className = "user-text";
+  const user_input_text = document.createTextNode(value);
+  user_input.appendChild(user_input_text);
+  b.appendChild(user_input);
 
+  const url = "http://34.162.110.229:5000";
+  const data = {
+    input: value,
+  };
 
-	const url = "http://34.162.110.229:5000";
-	const data = {
-		input: value,
-	};
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const json = await response.json();
 
-	const response = await fetch(url, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	});
-	const json = await response.json();
-
-	const a = document.getElementsByClassName("container")[0];
-	const div = document.createElement("div");
-	div.className = "ai-text";
-	const node = document.createTextNode(json.data);
-	div.appendChild(node);
-	a.appendChild(div);
+  const a = document.getElementsByClassName("chat-container")[0];
+  const div = document.createElement("div");
+  div.className = "ai-text";
+  const node = document.createTextNode(json.data);
+  div.appendChild(node);
+  a.appendChild(div);
 };
+
+// const aa = document.getElementsByClassName("chat-container")[0];
+// aa.innerHTML = `          <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">Fuck Dheeraj</div>
+// <div class="user-text">
+//   Dheeraj is an asshole
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">Fuck Dheeraj</div>
+// <div class="user-text">
+//   Dheeraj is an asshole
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">wefewufuef eiuferfggfyr eyfge8fe78f 8w7efew</div>
+// <div class="user-text">
+//   dfcgsahdsafguyefgysueafuytefuyte asefyguewgrewyfgyeufgyeuwfyuefgyue
+//   weyufgewyfyuewfewfew
+// </div>
+// <div class="ai-text">Fuck Dheeraj</div>
+// <div class="user-text">
+//   Dheeraj is an asshole
+// </div>
+// `;
