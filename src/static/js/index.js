9a92647490f05ea3e2c5d6a1ac8f492c487dadbd
dheeraj sai Gogineni <39270552+gogineni1998@users.noreply.github.com>
2023-12-09 17:39:20 -0500

    updated code

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,11 @@
-const my_input = async () => {
+let chart_generator = ""
+topic_data = [300, 50, 100, 10, 30, 23, 45, 67, 89, 90]
+document.getElementsByClassName("topic_names")[0].style.display = 'none'
+document.getElementsByClassName("chart")[0].style.display = 'none'
+const ctx = document.getElementById('myChart');
+topics_selected = []
 
+const my_input = async () => {
   value = document.getElementsByClassName("input-box")[0].value;
   document.getElementsByClassName("input-box")[0].value = "";
 
@@ -37,9 +43,11 @@ const my_input = async () => {
   // adding user input to loaded container
   chat_container.appendChild(user_input);
 
+
   const url = "http://34.162.110.229:5000";
   const data = {
     input: value,
+    topics: topics_selected
   };
 
   // creating loading div
@@ -70,7 +78,7 @@ const my_input = async () => {
 
   // removing loading div
   const element = document.getElementsByClassName("ai-text");
-  element[element.length-1].remove()
+  element[element.length - 1].remove()
 
   // creating ai generated div
   const div = document.createElement("div");
@@ -88,3 +96,89 @@ const my_input = async () => {
   // adding to main container
   chat_container.appendChild(div);
 };
+
+const topic_checker = () => {
+  let checkbox = document.getElementById("select_topics");
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      document.getElementsByClassName("topic_names")[0].style.display = 'block'
+      document.getElementById("select_analytics").style.display = 'none'
+      document.getElementById("analytics_label").style.display = 'none'
+    } else {
+      document.getElementsByClassName("topic_names")[0].style.display = 'none'
+      document.getElementById("select_analytics").style.display = 'inline'
+      document.getElementById("analytics_label").style.display = 'inline'
+    }
+  });
+}
+
+const analytics_checker = () => {
+  topic_data[0] = topic_data[0] + 1
+  let checkbox = document.getElementById("select_analytics");
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      document.getElementById("select_topics").style.display = 'none'
+      document.getElementById("topics_label").style.display = 'none'
+      document.getElementsByClassName("container")[0].style.display = 'none'
+      document.getElementsByClassName("input-container")[0].style.display = 'none'
+      document.getElementsByClassName("chart")[0].style.display = 'block'
+      document.getElementById("myChart").style.display = 'block'
+      if(chart_generator != "") {
+        chart_generator.destroy()
+      }
+      chart_generator = new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: [
+            'Health',
+            'Environment',
+            'Technology',
+            'Economy',
+            'Entertainment',
+            'Sports',
+            'Politics',
+            'Education',
+            'Travel',
+            'Food'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: topic_data,
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+              'rgb(255, 0, 0)',
+              'rgb(165, 42, 42)',
+              'rgb(145, 56, 49)',
+              'rgb(255, 68, 51)',
+              'rgb(222, 49, 99)',
+              'rgb(220, 20, 60)',
+              'rgb(169, 92, 104)'
+            ],
+            hoverOffset: 4
+          }]
+        }
+      });
+    } else {
+      document.getElementById("select_topics").style.display = 'inline'
+      document.getElementById("topics_label").style.display = 'inline'
+      document.getElementsByClassName("container")[0].style.display = 'block'
+      document.getElementsByClassName("input-container")[0].style.display = 'flex'
+      document.getElementsByClassName("chart")[0].style.display = 'none'
+      document.getElementById("myChart").style.display = 'none'
+    }
+  });
+  
+}
+
+const topic_adder = (topic) => {
+  let checkbox = document.getElementById(topic);
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      topics_selected.push(topic)
+    } else {
+      topics_selected = topics_selected.filter(element => element != topic)
+    }
+  });
+}
